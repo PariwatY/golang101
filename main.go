d9a96107f74d28255468bea9b2a6602d4d3d9dec
Pariwat Yimpayoon <32978747+PariwatY@users.noreply.github.com>
2022-04-09 20:28:00 +0700

    add insert update delete

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -21,6 +22,25 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//Insert Person with name
+	// err = AddPerson("pek001")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	//Update Person by id
+	// err = UpdatePerson(Person{5, "pek0002233"})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	//Delete Person by id
+	// err = DeletePerson(5)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	//Get all person
 	persons, err := GetPerson()
 	if err != nil {
 		panic(err)
@@ -29,6 +49,13 @@ func main() {
 	for _, person := range persons {
 		fmt.Println(person)
 	}
+
+	// person, err := GetPersonWithId(2)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Print(person)
 
 }
 
@@ -79,4 +106,85 @@ func GetPerson() ([]Person, error) {
 	}
 
 	return persons, err
+}
+
+func GetPersonWithId(id int) (*Person, error) {
+	err := db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	sql := db.QueryRow("select person_id, person_name from person where person_id = ?", id)
+
+	person := Person{}
+
+	err = sql.Scan(&person.id, &person.name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &person, nil
+}
+
+func AddPerson(namePerson string) error {
+
+	query := "insert into person (person_name) values (?)"
+
+	result, err := db.Exec(query, namePerson)
+	if err != nil {
+		return err
+	}
+
+	affect, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if affect <= 0 {
+		return errors.New("can't insert'")
+	}
+
+	return nil
+}
+
+func UpdatePerson(person Person) error {
+
+	query := "update person set person_name = ? where person_id = ?"
+
+	result, err := db.Exec(query, person.name, person.id)
+	if err != nil {
+		return err
+	}
+
+	affect, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if affect <= 0 {
+		return errors.New("can't update'")
+	}
+
+	return nil
+}
+
+func DeletePerson(id int) error {
+
+	query := "delete from person where person_id = ?"
+
+	result, err := db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	affect, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if affect <= 0 {
+		return errors.New("can't delete'")
+	}
+
+	return nil
 }
